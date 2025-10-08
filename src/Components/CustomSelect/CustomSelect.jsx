52d9300457d33571b5553428f92b305ed0181002
Faiq Sindhu <sindhufaiq555@gmail.com>
2025-10-08 16:00:37 +0500

    some changes

--- src/Components/CustomSelect/CustomSelect.jsx
+++ src/Components/CustomSelect/CustomSelect.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "./CustomSelect.module.css";
 
-const CustomSelect = ({ name, options }) => {
+const CustomSelect = ({ name, options, ref }) => {
+
+  
+
   return (
     <div className="bg-white text-gray-500 rounded-md text-lg">
       <select
+        ref={ref}
         className={`${styles.select} p-5 px-7 outline-none rounded-md w-full`}
         name={name}
         id=""
       >
         {options.map((opt) => (
-          <option key={opt} value={opt}>{opt}</option>
+          <option key={opt} value={opt}>
+            {opt}
+          </option>
         ))}
       </select>
     </div>
--- src/Components/HeroSearchForm.jsx
+++ src/Components/HeroSearchForm.jsx
@@ -1,27 +1,41 @@
-import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import CustomSelect from "./CustomSelect/CustomSelect";
+import { useRef, useState } from "react";
 
 const HeroSearchForm = (props) => {
+
+  const cityRef = useRef(null);
+  const phaseRef = useRef(null);
+  const typeRef = useRef(null);
+  const categoryRef = useRef(null);
+  const [keywordValue, setKeywordValue] = useState("");
+
   const cityOptions = ["All Cities","Lahore", "Multan", "Quetta", "Karachi"];
   const phaseOptions = ["All Phases", "Phase 1", "Phase 2"];
-  const typeOptions = ["Sale", "Rent", "Required"];
-  const categoryOptions = ["Plot", "House", "Apartment", "Villa"];
-
+  const typeOptions = ["All Property Types","Sale", "Rent", "Required"];
+  const categoryOptions = ["All Categories","Plot", "House", "Apartment", "Villa"];
 
   const submitHandler = (e) => {
     e.preventDefault();
+    const queryData = {
+      city: cityRef.current.value,
+      phase: phaseRef.current.value,
+      type: typeRef.current.value,
+      category: categoryRef.current.value,
+      keywordValue
+    };
 
+    console.log(queryData);
   }
 
   return (
     <form className={`text-[${props.color}] grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 w-full gap-4`} onSubmit={(e) => {submitHandler(e)}}>
 
-        <CustomSelect name={"city"} options={cityOptions}/>
-        <CustomSelect name={"phase"} options={phaseOptions}/>
-        <CustomSelect name={"type"} options={typeOptions}/>
-        <CustomSelect name={"category"} options={categoryOptions}/>
-        <input className="bg-white outline-none p-5 px-7 rounded-md" type="text" placeholder="Keyword" />
+        <CustomSelect ref={cityRef} name={"city"} options={cityOptions}/>
+        <CustomSelect ref={phaseRef} name={"phase"} options={phaseOptions}/>
+        <CustomSelect ref={typeRef} name={"type"} options={typeOptions}/>
+        <CustomSelect ref={categoryRef} name={"category"} options={categoryOptions}/>
+        <input onChange={(e) => setKeywordValue(e.target.value)} value={keywordValue} name="keyword" className="bg-white outline-none p-5 px-7 rounded-md" type="text" placeholder="Keyword" />
 
       <button className="bg-[#274abb] text-2xl rounded-md p-5 px-7 cursor-pointer flex justify-center" type="submit">
         <IoSearch color={`${props.color}`} />
