2f3dd9e0c6b4c03c75800184cebebc061cc6d0e4
Faiq Sindhu <sindhufaiq555@gmail.com>
2025-10-11 10:02:08 +0500

    changes to cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://dha-connect.netlify.app/'], // Add your frontend URLs
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://dha-connect.netlify.app'], // Add your frontend URLs
   credentials: true
 }));
 
