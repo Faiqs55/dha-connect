2f2dd44679bce33517684d914ffa81c44de882cf
Faiq Sindhu <sindhufaiq555@gmail.com>
2025-10-19 16:51:11 +0500

    file name change

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import TopBar from "@/components/Topbar";
+import TopBar from "./Topbar";
 import ContainerCenter from "./ContainerCenter";
 import logo from "@/assets/dha-connect-logo.png";
 import Image from "next/image";
