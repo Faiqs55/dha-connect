f551740debe06d383fa0cdb310ced462517aa388
Faiq Sindhu <sindhufaiq555@gmail.com>
2025-10-28 10:01:04 +0500

    property router updated

--- backend/src/routes/property.routes.js
+++ backend/src/routes/property.routes.js
@@ -4,11 +4,11 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const propertyRouter = express.Router();
 
-propertyRouter.use("/")
+propertyRouter.route("/")
 .get(getAllProperties)
 .post(protect, createPropertyController);
 
-propertyRouter.use("/:id")
+propertyRouter.route("/:id")
 .get(getSinglePropertyComtroller)
 .put(protect, updatePropertyController)
 .delete(protect, deletePropertyController);
